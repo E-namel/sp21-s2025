7a1c280cc766700d6ba97835dfdbe20b27e246ac
E-namel <cbb.luigi@gmail.com>
2025-09-15 01:02:43 +0800

    Complete randomizedtest in Lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,55 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        BuggyAList<Integer> testBuggyAList = new BuggyAList<>();
+        AListNoResizing<Integer> testAListNoResizing = new AListNoResizing<>();
+
+        int[] values = {4, 5, 6};
+        for (int v : values) {
+            testBuggyAList.addLast(v);
+            testAListNoResizing.addLast(v);
+        }
+
+        for (int i = 0; i < values.length; i++) {
+            assertEquals(testBuggyAList.removeLast(), testAListNoResizing.removeLast());
+        }
+
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            switch (operationNumber) {
+                case 0:
+                    // addLast
+                    int randVal = StdRandom.uniform(0, 100);
+                    L.addLast(randVal);
+                    B.addLast(randVal);
+                    break;
+                case 1:
+                    // size
+                    assertEquals(L.size(), B.size());
+                    break;
+                case 2:
+                    // removeLast
+                    if (L.size() > 0 || B.size() > 0) {
+                        assertEquals(L.removeLast(), B.removeLast());
+                    }
+                    break;
+                case 3:
+                    // getLast
+                    if (L.size() > 0 || B.size() > 0) {
+                        assertEquals(L.getLast(), B.getLast());
+                    }
+                    break;
+            }
+        }
+    }
 }
